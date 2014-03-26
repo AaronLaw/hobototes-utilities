8ec875fefd79bf61bafffb8dd7b50652bf374d43
Aaron Law <aaronlaw@gmail.com>
2014-03-27 01:36:36 +0800

    - clean up the pseduo code

--- many postage rates.py
+++ many postage rates.py
@@ -5,26 +5,8 @@
 # Ref: http://code.google.com/p/zhpy/wiki/DataStructure
 ######################
 # PSEUDO CODE: 同時show 幾個不同取貨地點的郵費，自己比較
-# ============
-# weight = raw_input
-# round weight to next integer
-# payment (weight):
-# first_kg + (weight - 1) * addition_kg + addition_fee
-
-# first_kg = 15
-# addition_kg = 5
-# addition_fee = 25
-# display payment: 'Payment of home...' = payment(weight)
-# first_kg = 15
-# addition_kg = 5
-# addition_fee = 0
-# display payment: 'Payment of factory...' = payment(weight)
-# first_kg = 10
-# addition_kg = 6
-# addition_fee = 0
-# display payment: 'Payment of self taken...' = payment(weight)
 ######################
-weight = int( input('Input the weight...') )
+weight = int( input('Input the weight (kg)...') )
 def payment(weight, costs):
 	return costs['first_kg'] + (weight - 1) * costs['addition_kg'] + costs['addition_fee']
 
