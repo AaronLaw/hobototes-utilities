612001351139533253883a7b1b141b755a2b3d2c
Aaron Law <aaronlaw@gmail.com>
2014-03-26 22:02:21 +0800

    decide to use Dictionary as a datastructure to organize the data of costs

--- many postage rates.py
+++ many postage rates.py
@@ -2,6 +2,7 @@
 # mass postage rates.py
 # 2014-03-25, by Aaronlaw
 # This program calculates postage rates of manybo
+# Ref: http://code.google.com/p/zhpy/wiki/DataStructure
 ######################
 # PSEUDO CODE: 同時show 幾個不同取貨地點的郵費，自己比較
 # ============
@@ -23,3 +24,15 @@
 # addition_fee = 0
 # display payment: 'Payment of self taken...' = payment(weight)
 ######################
+weight = input('Input the weight...')
+def payment(weight, costs):
+	costs['first_kg'] + (weight - 1) * costs['addition_kg'] + costs['addition_fee']
+
+costs = {'first_kg':15, 'addition_kg':5, 'addition_fee':25}
+print ('送去家需付... %s' % payment(weight, costs) )
+
+costs = {'first_kg':15, 'addition_kg':5, 'addition_fee':25}
+print ('送去公司需付...%s' % payment(weight, costs) )
+
+costs = {'first_kg':15, 'addition_kg':5, 'addition_fee':25}
+print ('自取需付...%s' % payment(weight, costs) )
